e2c7523db44f83dde9c27129896b5ed5f9cd44a6
LulianoM <martinslucianofigueira@gmail.com>
2025-03-23 15:06:09 -0300

    fix jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts', 'tests/unit/**/*.test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
