51591f5d3727d9dfe1ee1228b6aacfd7593635d9
lucianom <martinslucianofigueira@gmail.com>
2025-06-16 16:33:55 -0300

    remove old code

--- old-near-protocol-rewards/src/types/collectors.ts
+++
@@ -1,9 +0,0 @@
-import { ConsoleLogger } from "../utils/logger";
-import { RateLimiter } from "../utils/rate-limiter";
-
-export interface GitHubCollectorConfig {
-  token: string;
-  repo: string;
-  logger: ConsoleLogger;
-  rateLimiter: RateLimiter;
-}
